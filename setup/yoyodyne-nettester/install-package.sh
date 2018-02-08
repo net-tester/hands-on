e28e02a0086abf5af575bef66ed5f3968767cb91
tjmtrhs <tjmtrhs+github@gmail.com>
2018-02-08 11:00:43 +0900

    add packages required by scenario steps

--- setup/yoyodyne-nettester/install-package.sh
+++ setup/yoyodyne-nettester/install-package.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# this script requires root privileges
 
-sudo apt update
-sudo apt install -y git ruby2.3 ruby2.3-dev rubygems-integration build-essential rake openvswitch-switch ethtool iproute2 libxml2-dev zlib1g-dev ntp ntpdate libsqlite3-dev curl
-sudo gem install bundle
+apt update
+apt install -y git ruby2.3 ruby2.3-dev rubygems-integration build-essential rake openvswitch-switch ethtool iproute2 libxml2-dev zlib1g-dev ntp ntpdate libsqlite3-dev
+apt install -y dnsmasq netcat curl openssl
+gem install bundle
 curl -sS https://raw.githubusercontent.com/net-tester/net-tester/develop/script/install.sh | sh
 
