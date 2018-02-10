05d6cf2d874cb1c460cd19c6e84724f812d2050a
tjmtrhs <tjmtrhs+github@gmail.com>
2018-02-10 15:13:35 +0900

    add polling logic for start api in boot script

--- setup/scenario/boot-vm.sh
+++ setup/scenario/boot-vm.sh
@@ -4,3 +4,20 @@
 for vm in global-vyos yoyodyne-vyos tajimax-nettester yoyodyne-nettester ; do
   virsh start $vm
 done
+
+# wait for starting api
+while : ; do
+  curl http://172.16.0.2:3000 1>/dev/null 2>&1
+  if [[ "$?" -eq 0 ]] ; then
+    break
+  else
+    printf "\rstarting NetTester APIs"
+    for i in {1..10} ; do
+      sleep 1
+      printf "."
+    done
+    printf "\rstarting NetTester APIs          "
+  fi
+done
+printf "\ndone\n"
+
